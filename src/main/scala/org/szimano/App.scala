166d0d188f92ac790d85f2f4b65b8fd021ebbb4e
szimano <szimano@szimano.org>
2012-01-10 21:27:02 +0100

    loads csv into H2 using squeryl

--- src/main/scala/org/szimano/App.scala
+++ src/main/scala/org/szimano/App.scala
@@ -2,16 +2,35 @@ package org.szimano
 
 import au.com.bytecode.opencsv.CSVReader
 import java.io.FileReader
+import org.squeryl.{Session, SessionFactory, Schema}
+import org.squeryl.adapters.H2Adapter
+
+import org.squeryl.annotations.Column
+
+import org.squeryl.PrimitiveTypeMode._
 
 /**
  * @author ${user.name}
  */
+
 object App {
-  
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
+
+  def foo(x: Array[String]) = x.foldLeft("")((a, b) => a + b)
+
+  def main(args: Array[String]) {
+    // initialize the DB
+    Class.forName("org.h2.Driver")
+
+    SessionFactory.concreteFactory = Some(() => Session.create(
+      java.sql.DriverManager.getConnection("jdbc:h2:mem:test1;DB_CLOSE_DELAY=-1"),
+      new H2Adapter
+    ))
+
+    transaction {
+      Library.create
+    }
+
+    println("Hello World!")
     println("concat arguments = " + foo(args))
 
     val fileName = args(0)
@@ -19,12 +38,44 @@ object App {
     println(fileName)
 
     val reader = new CSVReader(new FileReader(fileName))
-    
-    var line = Array[String]()
-    
-    while((line = reader.readNext()) != null) {
-      println(foo(line))
+
+    var line = reader.readNext()
+
+    println("wczytuje leki")
+
+    while (line != null) {
+
+      transaction {
+        Library.leki.insert(new Lek(augmentString(line(0)).toLong, line(1), line(2)))
+      }
+      line = reader.readNext()
     }
+
+    println("wczytalem leki")
+
+    transaction {
+      def leki = from(Library.leki)(l => where(l.substancja like "Viga%") select(l))
+
+      println("Znalazlem " + leki.size + " lekow")
+      for (l <- leki)
+        println(l)
+    }
+
   }
 
+}
+
+
+class Lek(val id: Long,
+          @Column(length = 500)
+          var substancja: String,
+          @Column(length = 500)
+          var nazwa: String) {
+  def this() = this (0, "", "")
+
+  override def toString() = "Lek[id="+id+";substancja="+substancja+";nazwa="+nazwa+"]"
+}
+
+object Library extends Schema {
+  val leki = table[Lek]("LEK")
 }
