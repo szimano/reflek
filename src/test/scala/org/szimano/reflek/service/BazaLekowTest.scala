ab92824c1eeaf1fdb208d6b71bed8720f2f7fef4
Tomasz Szymanski <szimano@Tomaszs-MacBook-Air.local>
2012-01-21 12:23:13 +0200

    filled in requirements of the tests

--- src/test/scala/org/szimano/reflek/service/BazaLekowTest.scala
+++ src/test/scala/org/szimano/reflek/service/BazaLekowTest.scala
@@ -13,13 +13,47 @@ import org.scalatest.testng.TestNGSuite
  */
 
 @RunWith(classOf[JUnitRunner])
-class BazaLekowTest extends FeatureSpec {
+class BazaLekowTest extends FeatureSpec with GivenWhenThen {
   feature ("User can search for drugs") {
-    scenario("All drugs are queried")(pending)
-    scenario("All drugs are queried on an empty DB")(pending)
-    scenario("Drugs with one search token are queried")(pending)
-    scenario("Drugs with two search tokens are queried")(pending)
-    scenario("Drugs with one search token are queried on an empty DB")(pending)
-    scenario("Drugs with two search tokens are queried on an empty DB")(pending)
+    info("As a user")
+    info("I want to be able to search for refunded drugs")
+    info("So I can easily find what I am looking for")
+
+    scenario("All drugs are queried") {
+      given("non-empty db")
+      when("all drugs are queried")
+      then("all drugs should be returned")
+      pending
+    }
+    scenario("All drugs are queried on an empty DB"){
+      given("empty db")
+      when("all drugs are queried")
+      then("no drugs should be returned")
+      pending
+    }
+    scenario("Drugs with one search token are queried"){
+      given("non-empty db")
+      when("drugs are queried with one search token")
+      then("all drugs with such token should be returned")
+      pending
+    }
+    scenario("Drugs with two search tokens are queried"){
+      given("non-empty db")
+      when("drugs are queried with two search tokens")
+      then("all drugs that have all the tokens should be returned")
+      pending
+    }
+    scenario("Drugs with one search token are queried on an empty DB"){
+      given("empty db")
+      when("drugs are queried with one search token")
+      then("no drugs should be returned")
+      pending
+    }
+    scenario("Drugs with two search tokens are queried on an empty DB"){
+      given("empty db")
+      when("drugs are queried with two search tokens")
+      then("no drugs should be returned")
+      pending
+    }
   }
 }
